caaac39334d108280a18b338ddfb0b0a0a4bc74f
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 21:22:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,4 +3,7 @@ def sort_array_asc(int_array)
   int_array.sort
 end
 
-#
+# Method that sorts an array of integers in descending order
+def sort_array_desc(int_array)
+  int_array.sort {|a, b| b <=> a}
+end
